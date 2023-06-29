c6ba74495416a5c54387376edcd6bd726bf5b41f
sombre-all1 <131678756+sombre-all1@users.noreply.github.com>
2023-06-29 13:45:47 +0000

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,14 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['23561963446', '🥀⃞⃕𝙎𝙊𝙈𝘽𝙍𝙀༆𝘿𝘼𝙀𝙈𝙊𝙉⟫❦⃞᭄
+', true],
+  ['17014093374', '𝑨𝒚𝒂𝒏𝒐𝒌𝒐𝒋𝒊✘𝑲𝒊𝒚𝒐𝒕𝒂𝒌𝒂℘', true], 
   [''] 
 ] //Number of owners
 
-global.mods = ['917605902011','32460248586'] 
-global.prems = ['917605902011', '32460248586', '919398758484']
+global.mods = ['23561963446','17014093374'] 
+global.prems = ['17014093374', '23561963446', '17014093374']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
